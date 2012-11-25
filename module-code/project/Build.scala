c57cf8cd124bb15a844e5f02286bae4c5bb78fc0
jorge <jaliss@gmail.com>
2012-11-25 17:21:04 -0300

    changed version to 2.0.7

--- module-code/project/Build.scala
+++ module-code/project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "securesocial"
-    val appVersion      = "master"
+    val appVersion      = "2.0.7"
 
     val appDependencies = Seq(
       "com.typesafe" %% "play-plugins-util" % "2.0.3",
