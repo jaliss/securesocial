b6d4387a9eec9e12643a0dd29c3b31f8562f61d3
jorge <jaliss@gmail.com>
2013-01-25 18:00:32 -0300

    updated version to 2.0.9

--- samples/java/demo/project/Build.scala
+++ samples/java/demo/project/Build.scala
@@ -8,10 +8,10 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-        "securesocial" % "securesocial_2.9.1" % "master"
+        "securesocial" % "securesocial_2.9.1" % "2.0.9"
     )
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      resolvers += Resolver.url("SecureSocial Repository", url("http://securesocial.ws/repository/snapshots/"))(Resolver.ivyStylePatterns)
+      resolvers += Resolver.url("SecureSocial Repository", url("http://securesocial.ws/repository/releases/"))(Resolver.ivyStylePatterns)
     )
 
 }
