6d33b528c86798ef2b5b1255402126b8092c796f
Mike Slinn <mslinn@micronauticsresearch.com>
2013-02-02 13:35:49 -0800

    This publishing setup works

--- module-code/project/Build.scala
+++ module-code/project/Build.scala
@@ -6,6 +6,11 @@ object ApplicationBuild extends Build {
   val appName         = "securesocial"
   val appVersion      = "2.1-RC4"
 
+  organization  := "com.micronautics" // Don't want this fork to step on securesocial
+
+  crossPaths    := false
+  publishMavenStyle := false
+
   val appDependencies = Seq(
     "com.typesafe" %% "play-plugins-util" % "2.1-RC2",   // not released for Play 2.1-RC4 yet
     "com.typesafe" %% "play-plugins-mailer" % "2.1-RC2", // not released for Play 2.1-RC4 yet
@@ -13,19 +18,11 @@ object ApplicationBuild extends Build {
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-  ).settings(
     resolvers ++= Seq(
       "jBCrypt Repository" at "http://repo1.maven.org/maven2/org/",
       "Typesafe Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
-    )
-  )
+    ),
 
-  publishTo <<= (version) { version: String =>
-     val scalasbt = "http://scalasbt.artifactoryonline.com/scalasbt/"
-     val (name, url) = if (version.contains("-SNAPSHOT"))
-                         ("sbt-plugin-snapshots", scalasbt+"sbt-plugin-snapshots")
-                       else
-                         ("sbt-plugin-releases", scalasbt+"sbt-plugin-releases")
-     Some(Resolver.url(name, new URL(url))(Resolver.ivyStylePatterns))
-  }
+    publishTo := Some(Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns))
+  )
 }
