b0c69bd65b2eb9273542d5efd0a4efe73e3b4206
Mike Slinn <mslinn@micronauticsresearch.com>
2013-02-02 22:56:07 -0800

    Sample app now works from public repo

--- samples/scala/demo/project/Build.scala
+++ samples/scala/demo/project/Build.scala
@@ -3,17 +3,18 @@ import Keys._
 import play.Project._
 
 object ApplicationBuild extends Build {
-
     val appName         = "ssdemo-scala"
     val appVersion      = "1.0-SNAPSHOT"
 
     scalaVersion := "2.10.0"
 
     val appDependencies = Seq(
-    	"securesocial" %% "securesocial" % "2.1-RC4" withSources()
+    	"com.micronautics" % "securesocial" % "2.1-RC4a" withSources()
     )
     val main = play.Project(appName, appVersion, appDependencies).settings(
-      resolvers += Resolver.url("SecureSocial Repository", url("http://securesocial.ws/repository/releases/"))(Resolver.ivyStylePatterns)
+      resolvers += Resolver.url("SecureSocial Repository", url("http://securesocial.ws/repository/releases/"))(Resolver.ivyStylePatterns),
+      resolvers += Resolver.url("play-plugin-releases", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns),
+      resolvers += Resolver.url("play-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
     )
 
 }
