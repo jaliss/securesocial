215c196115c8cfc3ca691f59a17f5d56d869b007
jorge <jaliss@gmail.com>
2013-02-25 14:48:03 -0300

    bump version for release.

--- samples/java/demo/project/Build.scala
+++ samples/java/demo/project/Build.scala
@@ -8,10 +8,10 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-        "securesocial" %% "securesocial" % "master"
+        "securesocial" %% "securesocial" % "2.0.12"
     )
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-	resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
+	resolvers += Resolver.url("sbt-plugin-releases", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
     )
 
 }
