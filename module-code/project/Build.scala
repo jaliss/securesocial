f00fa83d887dab6328db1cd74542691c42150d5c
Jorge <jaliss@example.com>
2013-08-07 15:40:49 -0300

    Set version to 2.0.13

--- module-code/project/Build.scala
+++ module-code/project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "securesocial"
-    val appVersion      = "2.0-SNAPSHOT"
+    val appVersion      = "2.0.13"
 
     val appDependencies = Seq(
       "com.typesafe" %% "play-plugins-util" % "2.0.3",
