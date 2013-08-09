feee6f3e916a615d5716c7be0e5c0935b6935771
FortyTwo <fortytwo@marvin.keep42.com>
2013-08-09 00:34:42 -0400

    change repo

--- module-code/project/Build.scala
+++ module-code/project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "securesocial"
-    val appVersion      = "master-SNAPSHOT"
+    val appVersion      = "master-20130808"
 
     val appDependencies = Seq(
       "com.typesafe" %% "play-plugins-util" % "2.1.0",
@@ -15,19 +15,11 @@ object ApplicationBuild extends Build {
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
     ).settings(
-      publishMavenStyle := false,
       resolvers ++= Seq(
         "jBCrypt Repository" at "http://repo1.maven.org/maven2/org/",
         "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
       ),
-      publishTo <<= (version) { version: String =>
-	val scalasbt = "http://repo.scala-sbt.org/scalasbt/"
-   	val (name, url) = if (version.contains("-SNAPSHOT"))
-     	  ("sbt-plugin-snapshots", scalasbt+"sbt-plugin-snapshots")
-   	else
-          ("sbt-plugin-releases", scalasbt+"sbt-plugin-releases")
-         Some(Resolver.url(name, new URL(url))(Resolver.ivyStylePatterns))
-      }
+      publishTo := Some(Resolver.file("file", new File("../../sonatype-work/nexus/storage/towel")))
     )
 
 }
