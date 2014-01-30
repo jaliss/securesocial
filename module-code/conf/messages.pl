8175c85fad1f5e80984301e0cd847e96af99f153
Jorge <jaliss@example.com>
2014-01-30 10:37:17 -0300

    - Added Polish translation (thanks Dominik Sienkiewicz) - Added Spanish translation (thanks @jorgeolmos) - Added Hungarian translation (thanks @bubbanat) - Added French translation (thanks @afigoureux) - Added Serbian translation (thanks @vitalije) - Added Hebrew translation (thanks @katrinsharp)

---
+++ module-code/conf/messages.pl
@@ -0,0 +1,65 @@
+# App name
+securesocial.appName=SecureSocial 2
+
+# Login page
+securesocial.login.title=Logowanie
+securesocial.login.instructions=U¿yj istniej¹cego konta z jednego z nastêpuj¹cych portali, aby siê zalogowaæ.
+securesocial.login.accessDenied=Masz zablokowany dostêp do konta. Nadaj uprawnienia by siê zalogowaæ.
+securesocial.login.errorLoggingIn=Wyst¹pi³ b³¹d podczas logowania. Proszê spróbowaæ ponownie.
+securesocial.login.useEmailAndPassword=Lub zaloguj za pomoc¹ nazwy u¿ytkownika i has³a.
+securesocial.login.useEmailAndPasswordOnly=Zaloguj siê przy u¿yciu nazwy u¿ytkownika i has³a.
+securesocial.login.signUp=Jeœli jeszcze nie masz u nas konta, mo¿esz za³o¿yæ je
+securesocial.login.here=tutaj
+securesocial.login.invalidCredentials=B³êdny login lub has³o
+securesocial.login.forgotPassword=Nie pamiêtasz swojego has³a?
+
+# Sign up page
+securesocial.signup.title=Rejestracja nowego konta
+securesocial.signup.username=Nazwa u¿ytkownika
+securesocial.signup.firstName=Imiê
+securesocial.signup.lastName=Nazwisko
+securesocial.signup.email1=Email
+securesocial.signup.email2=Powtórz Email
+securesocial.signup.password1=Has³o
+securesocial.signup.password2=Powtórz has³o
+securesocial.signup.createAccount=Utwórz konto 
+securesocial.signup.cancel=Anuluj
+securesocial.signup.userNameAlreadyTaken=Nazwa jest ju¿ zajêta
+securesocial.signup.passwordsDoNotMatch=Has³a nie s¹ takie same
+securesocial.signup.thankYouCheckEmail=Dziêkujemy. Prosimy sprawdziæ e-mail dla dalszych instrukcji
+securesocial.signup.invalidLink=Link, który wybra³eœ jest nieprawid³owy
+securesocial.signup.signUpDone=Dziêkujemy za rejestracjê. Mo¿esz siê teraz zalogowaæ
+securesocial.signup.invalidPassword=Podaj przynajmniej {0} znaków
+
+# Password reset
+securesocial.password.title=Resetowanie has³a
+securesocial.password.passwordUpdated=Twoje has³o zosta³o pomyœlnie zmienione
+securesocial.password.reset=Zrestartuj has³o
+securesocial.password.error=Wyst¹pi³ problem przy zmianie has³a. Proszê spróbowaæ ponownie
+
+# Password change
+securesocial.passwordChange.title=Zmiana has³a
+securesocial.passwordChange.currentPassword=Aktualne has³o
+securesocial.passwordChange.newPassword1=Nowe has³o
+securesocial.passwordChange.newPassword2=Powtórz nowe has³o
+securesocial.passwordChange.changeButton=Zmieñ
+securesocial.passwordChange.invalidPassword=Nieprawid³owe has³o 
+securesocial.passwordChange.ok=Twoje has³o zosta³o pomyœlnie zmienione
+securesocial.passwordChange.okButton=Ok
+
+# Not authorized page
+securesocial.notAuthorized.title=Brak uprawnieñ
+securesocial.notAuthorized.message=Nie masz uprawnieñ, aby uzyskaæ dostêp do tej strony
+
+#
+securesocial.loginRequired=Musisz siê zalogowaæ, aby uzyskaæ dostêp do tej strony.
+
+# Mails
+#
+mails.sendAlreadyRegisteredEmail.subject=Instrukcja rejestracji nowego konta
+mails.sendSignUpEmail.subject=Instrukcja rejestracji nowego konta
+mails.welcomeEmail.subject=Witaj
+mails.passwordResetEmail.subject=Instrukcja zrestartowania has³a
+mails.unknownEmail.subject=¯¹danie zresetowania has³a
+mails.passwordResetOk.subject=Potwierdzenie zmiany has³a
+
