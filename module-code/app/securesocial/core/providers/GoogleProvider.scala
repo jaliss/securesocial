c2c6d67f12eb4cd0a4fe1683a4a82ecce7c50d0a
jorge <jaliss@gmail.com>
2014-08-11 17:26:26 -0300

    Updated GoogleProvider to use the new Google endpoints

--- module-code/app/securesocial/core/providers/GoogleProvider.scala
+++ module-code/app/securesocial/core/providers/GoogleProvider.scala
@@ -16,7 +16,7 @@
  */
 package securesocial.core.providers
 
-import play.api.libs.json.JsObject
+import play.api.libs.json.{JsArray, JsObject}
 import securesocial.core._
 import securesocial.core.services.{CacheService, RoutesService}
 
@@ -31,16 +31,22 @@ class GoogleProvider(routesService: RoutesService,
                      client: OAuth2Client)
   extends OAuth2Provider(routesService, client, cacheService)
 {
-  val UserInfoApi = "https://www.googleapis.com/oauth2/v1/userinfo?access_token="
+  val UserInfoApi = "https://www.googleapis.com/plus/v1/people/me?fields=id,name,displayName,image,emails&access_token="
   val Error = "error"
   val Message = "message"
-  val Type = "type"
+  val Code = "code"
   val Id = "id"
   val Name = "name"
-  val GivenName = "given_name"
-  val FamilyName = "family_name"
-  val Picture = "picture"
-  val Email = "email"
+  val GivenName = "givenName"
+  val FamilyName = "familyName"
+  val DisplayName = "displayName"
+  val Image = "image"
+  val Url = "url"
+  val Emails = "emails"
+  val Email = "value"
+  val EmailType = "type"
+  val Account = "account"
+
 
 
   override val id = GoogleProvider.Google
@@ -52,17 +58,17 @@ class GoogleProvider(routesService: RoutesService,
         (me \ Error).asOpt[JsObject] match {
           case Some(error) =>
             val message = (error \ Message).as[String]
-            val errorType = (error \ Type).as[String]
-            logger.error("[securesocial] error retrieving profile information from Google. Error type = %s, message = %s"
-              .format(errorType, message))
+            val errorCode = (error \ Code).as[String]
+            logger.error(s"[securesocial] error retrieving profile information from Google. Error type = $errorCode, message = $message")
             throw new AuthenticationException()
           case _ =>
             val userId = (me \ Id).as[String]
-            val firstName = (me \ GivenName).asOpt[String]
-            val lastName = (me \ FamilyName).asOpt[String]
-            val fullName = (me \ Name).asOpt[String]
-            val avatarUrl = (me \ Picture).asOpt[String]
-            val email = (me \ Email).asOpt[String]
+            val firstName = (me \ Name \ GivenName).asOpt[String]
+            val lastName = (me \ Name \ FamilyName).asOpt[String]
+            val fullName = (me \ DisplayName).asOpt[String]
+            val avatarUrl = (me \ Image \ Url).asOpt[String]
+            val emails = (me \ Emails).asInstanceOf[JsArray]
+            val email = emails.value.find(v => (v \ EmailType).as[String] == Account).map( e => (e \ Email).as[String])
             BasicProfile(id, userId, firstName, lastName, fullName, email, avatarUrl, authMethod, oAuth2Info = Some(info))
         }
     } recover {
