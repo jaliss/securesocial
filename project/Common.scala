afa3ae2086bda5bd3f0db2b301423517e9813b0a
jorge <jaliss@gmail.com>
2014-08-13 17:15:55 -0300

    Set version to 3.0-M1

--- project/Common.scala
+++ project/Common.scala
@@ -2,7 +2,7 @@ import sbt._
 import Keys._
 
 object Common {
-  def version = "master-SNAPSHOT"  
+  def version = "3.0-M1"
   def playVersion = System.getProperty("play.version", "2.3.1")
   def scalaVersion =  System.getProperty("scala.version", "2.11.1")
 }
