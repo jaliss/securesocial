5f00c440acb47cd6b539d333eb93ce562c1510da
jorge <jaliss@gmail.com>
2014-08-22 14:25:10 -0300

    Switched back to Play 2.3.2 because there are issues publishing signed artifacts with 2.3.3

--- project/Common.scala
+++ project/Common.scala
@@ -3,6 +3,6 @@ import Keys._
 
 object Common {
   def version = "master-SNAPSHOT"  
-  def playVersion = System.getProperty("play.version", "2.3.3")
+  def playVersion = System.getProperty("play.version", "2.3.2")
   def scalaVersion =  System.getProperty("scala.version", "2.11.1")
 }
