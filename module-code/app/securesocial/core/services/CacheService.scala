a7d31f7020221ae18c3103d2ffef6763f425f6da
jorge <jaliss@gmail.com>
2014-10-14 18:06:59 -0300

    Fixed: CacheService.Default was ignoring ttl

--- module-code/app/securesocial/core/services/CacheService.scala
+++ module-code/app/securesocial/core/services/CacheService.scala
@@ -43,7 +43,7 @@ object CacheService {
     import play.api.Play.current
 
     override def set[T](key: String, value: T, ttlInSeconds: Int): Future[Unit] =
-      Future.successful(Cache.set(key, value))
+      Future.successful(Cache.set(key, value, ttlInSeconds))
 
     override def getAs[T](key: String)(implicit ct: ClassTag[T]): Future[Option[T]] = Future.successful {
       Cache.getAs[T](key)
