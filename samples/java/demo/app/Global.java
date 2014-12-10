961a6ea01effd13ab1046fff9a04e77a28cb3d6e
jorge <jaliss@gmail.com>
2014-12-10 20:05:22 -0300

    Fixed #501: Requests get redirected to wrong routes (Java)

--- samples/java/demo/app/Global.java
+++ samples/java/demo/app/Global.java
@@ -16,29 +16,21 @@
  */
 
 import play.GlobalSettings;
-import play.Logger;
 import securesocial.core.RuntimeEnvironment;
 import service.MyEnvironment;
 
-import java.util.HashMap;
-
 public class Global extends GlobalSettings {
     private RuntimeEnvironment env = new MyEnvironment();
-    private HashMap<String, Object> instances = new HashMap<String, Object>();
 
     @Override
     public <A> A getControllerInstance(Class<A> controllerClass) throws Exception {
-        A result = (A) instances.get(controllerClass.getName());
-        Logger.debug("result = " + result);
-        if ( result == null ) {
-            Logger.debug("creating controller: " + controllerClass.getName());
-            try {
-                result = controllerClass.getDeclaredConstructor(RuntimeEnvironment.class).newInstance(env);
-            } catch (NoSuchMethodException e) {
-                // the controller does not receive a RuntimeEnvironment, delegate creation to base class.
-                result = super.getControllerInstance(controllerClass);
-            }
-            instances.put(controllerClass.getName(), result);
+        A result;
+        
+        try {
+            result = controllerClass.getDeclaredConstructor(RuntimeEnvironment.class).newInstance(env);
+        } catch (NoSuchMethodException e) {
+            // the controller does not receive a RuntimeEnvironment, delegate creation to base class.
+            result = super.getControllerInstance(controllerClass);
         }
         return result;
     }
