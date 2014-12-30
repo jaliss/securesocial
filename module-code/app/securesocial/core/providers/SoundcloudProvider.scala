39066df5af3bf0b510401571af1197af21f38b2b
jorge <jaliss@gmail.com>
2014-12-30 15:07:28 -0300

    Added copyright notice to Soundcould provider

--- module-code/app/securesocial/core/providers/SoundcloudProvider.scala
+++ module-code/app/securesocial/core/providers/SoundcloudProvider.scala
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2014 @amertum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package securesocial.core.providers
 
 import play.api.libs.json.JsObject
@@ -5,7 +21,6 @@ import securesocial.core._
 import securesocial.core.services.{ CacheService, RoutesService }
 
 import scala.concurrent.Future
-import scala.util.{ Success, Failure }
 
 /**
  * A SoundcloudProvider OAuth2 Provider
