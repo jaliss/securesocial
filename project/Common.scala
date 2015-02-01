d2cdca168ca33ab4eef87958d876bb4e5f71efd5
jorge <jaliss@gmail.com>
2015-02-01 20:12:55 -0300

    releasing 3.0-M3

--- project/Common.scala
+++ project/Common.scala
@@ -1,7 +1,7 @@
 
 
 object Common {
-  def version = "master-SNAPSHOT"  
+  def version = "3.0-M3"
   def playVersion = System.getProperty("play.version", "2.3.7")
   def scalaVersion =  System.getProperty("scala.version", "2.11.1")
 }
