0e574ba90bf94734867e71bc894be4b4855063f5
Leonid Vygovskiy <leonid.vygovskiy@gmail.com>
2015-02-04 02:05:12 +0300

    Version is changed to 1.0-SNAPSHOT

--- project/Common.scala
+++ project/Common.scala
@@ -2,7 +2,7 @@ import sbt._
 import Keys._
 
 object Common {
-  def version = "0.1"
+  def version = "1.0-SNAPSHOT"
   def playVersion = System.getProperty("play.version", "2.3.7")
   def scalaVersion =  System.getProperty("scala.version", "2.11.4")
 }
