0627c915bcd1ddff9d098cf1cab350c2eabc2881
Joe Conley <josephpconley@gmail.com>
2015-02-03 16:22:58 -0500

    working version of reddit provider

--- module-code/app/securesocial/core/providers/RedditProvider.scala
+++ module-code/app/securesocial/core/providers/RedditProvider.scala
@@ -1,6 +1,9 @@
 package securesocial.core.providers
 
-import play.api.libs.ws.WSResponse
+import org.apache.commons.codec.binary.Base64
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import play.api.mvc.Request
 import securesocial.core._
 import securesocial.core.services.{ CacheService, RoutesService }
 
@@ -11,53 +14,52 @@ import scala.concurrent.Future
  *
  */
 class RedditProvider(routesService: RoutesService,
-  cacheService: CacheService,
-  client: OAuth2Client)
+    cacheService: CacheService,
+    client: OAuth2Client)
     extends OAuth2Provider(routesService, client, cacheService) {
-  val GetAuthenticatedUser = "https://www.reddit.com/api/v1/me?access_token=%s"
-  val AccessToken = "access_token"
-  val TokenType = "token_type"
-  val Message = "message"
+  val MeUrl = "https://oauth.reddit.com/api/v1/me"
   val Id = "id"
   val Name = "name"
-  val AvatarUrl = "avatar_url"
-  val Email = "email"
 
   override val id = RedditProvider.Reddit
 
-  override protected def buildInfo(response: WSResponse): OAuth2Info = {
-    val values: Map[String, String] = response.body.split("&").map(_.split("=")).withFilter(_.size == 2)
-      .map(r => (r(0), r(1)))(collection.breakOut)
-    val accessToken = values.get(OAuth2Constants.AccessToken)
-    if (accessToken.isEmpty) {
-      logger.error(s"[securesocial] did not get accessToken from $id")
-      throw new AuthenticationException()
+  override protected def getAccessToken[A](code: String)(implicit request: Request[A]): Future[OAuth2Info] = {
+    val callbackUrl = routesService.authenticationUrl(id)
+    val params = Map(
+      OAuth2Constants.GrantType -> Seq(OAuth2Constants.AuthorizationCode),
+      OAuth2Constants.Code -> Seq(code),
+      OAuth2Constants.RedirectUri -> Seq(callbackUrl)
+    ) ++ settings.accessTokenUrlParams.mapValues(Seq(_))
+
+    val creds = s"${settings.clientId}:${settings.clientSecret}"
+    val enc = new String(Base64.encodeBase64(creds.getBytes()))
+    val basicAuth = s"Basic $enc"
+
+    client.httpService.url(settings.accessTokenUrl).withHeaders("Authorization" -> basicAuth).post(params).map(buildInfo)
+        .recover {
+      case e =>
+        logger.error("[securesocial] error trying to get an access token for provider %s".format(id), e)
+        throw new AuthenticationException()
     }
-    OAuth2Info(
-      accessToken.get,
-      values.get(OAuth2Constants.TokenType),
-      values.get(OAuth2Constants.ExpiresIn).map(_.toInt),
-      values.get(OAuth2Constants.RefreshToken)
-    )
   }
+
+  val userInfoReader = (
+      (__ \ Id).read[String] and
+          (__ \ Name).read[String]
+      ).tupled
 
   def fillProfile(info: OAuth2Info): Future[BasicProfile] = {
     import scala.concurrent.ExecutionContext.Implicits.global
-    client.retrieveProfile(GetAuthenticatedUser.format(info.accessToken)).map { me =>
-
-      logger.info(me.toString())
-
-      (me \ Message).asOpt[String] match {
-        case Some(msg) =>
-          logger.error(s"[securesocial] error retrieving profile information from Reddit. Message = $msg")
+    client.httpService.url(MeUrl).withHeaders("Authorization" -> s"bearer ${info.accessToken}").get().map { res =>
+      userInfoReader.reads(res.json).fold(
+        invalid => {
+          logger.error("[securesocial] got back " + res.body)
           throw new AuthenticationException()
-        case _ =>
-          val userId = (me \ Id).as[Int]
-          val displayName = (me \ Name).asOpt[String]
-          val avatarUrl = (me \ AvatarUrl).asOpt[String]
-          val email = (me \ Email).asOpt[String].filter(!_.isEmpty)
-          BasicProfile(id, userId.toString, None, None, displayName, email, avatarUrl, authMethod, oAuth2Info = Some(info))
-      }
+        },
+        me => {
+          BasicProfile(id, me._1, None, None, Some(me._2), None, None, authMethod, oAuth2Info = Some(info))
+        }
+      )
     } recover {
       case e: AuthenticationException => throw e
       case e =>
