645e57de9b0512ec28e17f0eea761a792716723e
jorge <jaliss@gmail.com>
2015-11-29 19:25:12 -0300

    upgraded to Play 2.4.4

--- project/Common.scala
+++ project/Common.scala
@@ -1,7 +1,7 @@
 
 
 object Common {
-  def version = "master-SNAPSHOT"  
-  def playVersion = System.getProperty("play.version", "2.4.2")
+  def version = "3.0-M4"
+  def playVersion = System.getProperty("play.version", "2.4.4")
   def scalaVersion =  System.getProperty("scala.version", "2.11.7")
 }
