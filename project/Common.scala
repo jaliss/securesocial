8fe2fbb44c32c8a3f6b6c5d1ea0a2d329748dadf
Jorge <jaliss@gmail.com>
2017-02-17 19:15:54 -0300

    Bump to 3.0-M6

--- project/Common.scala
+++ project/Common.scala
@@ -1,7 +1,7 @@
 
 
 object Common {
-  def version = "master-SNAPSHOT"  
+  def version = "3.0-M6"
   def playVersion = System.getProperty("play.version", "2.5.12")
   def scalaVersion =  System.getProperty("scala.version", "2.11.7")
 }
