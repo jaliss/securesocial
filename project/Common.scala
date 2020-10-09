8d07914e3e9695ead7ca19ca63301c66ad94f6fd
Wilton Risenhoover <risenhoover@gmail.com>
2020-10-09 14:56:42 -0700

    Bump scala version

--- project/Common.scala
+++ project/Common.scala
@@ -3,6 +3,6 @@
 object Common {
   def version = "master-SNAPSHOT"  
   def playVersion = System.getProperty("play.version", "2.6.25")
-  def scalaVersion = System.getProperty("scala.version", "2.12.2")
+  def scalaVersion = System.getProperty("scala.version", "2.12.12")
   def crossScalaVersions = Nil
 }
