61e1603a1fbc40b934e37a1f78883dec7a71c47f
Wilton Risenhoover <risenhoover@gmail.com>
2020-10-09 14:52:14 -0700

    Fix versions

--- project/Common.scala
+++ project/Common.scala
@@ -4,5 +4,5 @@ object Common {
   def version = "master-SNAPSHOT"  
   def playVersion = System.getProperty("play.version", "2.6.25")
   def scalaVersion = System.getProperty("scala.version", "2.12.2")
-  def crossScalaVersions = Seq(scalaVersion, "2.11.12")
+  def crossScalaVersions = Nil
 }
